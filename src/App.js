a11818ad51d7f61d04923e2f74aa726ea23417b9
hungpq96 <quanghwng22@gmail.com>
2021-12-15 15:21:05 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from "react"
+
+import AnticTemplate from "./pages/AnticTemplate"
+import BlogTemplate from "./pages/BlogTemplate"
+
 export default function App() {
+  const contents = {
+    '/': BlogTemplate,
+    '/antic': AnticTemplate,
+  }
+  const { pathname } = window?.location
+  const [PageComponent, setPageComponent] = useState(contents['/'])
+
+  useEffect(() => {
+    setPageComponent(contents[pathname])
+  }, [pathname])
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main>
+      {PageComponent}
+    </main>
   )
 }
